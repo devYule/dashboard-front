af6fcb9eb45dcb10a7c3356e22454c2714ee9ec3
devYule <privateDevYule@gmail.com>
2024-04-12 22:33:23 +0900

    2024-04-12 반응형, sticky  -- by Hyunmin

--- src/pages/main/sidebar/SidebarContainer.tsx
+++ src/pages/main/sidebar/SidebarContainer.tsx
@@ -5,13 +5,15 @@ import { SidebarActionContext, SidebarDirectionContext, SidebarSelectedItemConte
 export default function SidebarContainer() {
 
     // sidebarAction from <MainContainer />
+    // 아래 상태들을 바탕으로 나타낼 컴포넌트들 결정.
     const sidebardAction = useContext(SidebarActionContext);
     const sidebarDirection = useContext(SidebarDirectionContext);
+    const sidebarSelectedItem = useContext(SidebarSelectedItemContext);
 
     return (
         <div className="sidebarContainer">
 
-            
+
         </div>
     )
 }
